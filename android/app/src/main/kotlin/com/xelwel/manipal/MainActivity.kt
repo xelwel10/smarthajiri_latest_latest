0f3e972b8a73664dfe1c03b29c71c1fa6b8969f3
xelwel10 <xelwelinnovations@gmail.com>
2025-03-20 11:48:46 +0545

    release

--- android/app/src/main/kotlin/com/xelwel/manipal/MainActivity.kt
+++ android/app/src/main/kotlin/com/xelwel/manipal/MainActivity.kt
@@ -3,23 +3,9 @@ package com.xelwel.manipal
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 class MainActivity: FlutterFragmentActivity() {
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-                .setMethodCallHandler(
-        (call, result) -> {
-            override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
-        }
-        );
-    }
-
 }
