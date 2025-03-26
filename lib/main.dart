5a8a6b866278d09c2547b3960cef1f04d2e74d74
xelwel10 <xelwelinnovations@gmail.com>
2025-03-26 12:42:22 +0545

    medibiz

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,8 @@ void main() async {
   // Config.setHomeUrl("https://hrm.gmc.edu.np");
   // Config.setHomeUrl("https://grdbl.xelwel.com");
   // Config.setHomeUrl("https://ehpl.xelwel.com");
-  Config.setHomeUrl("https://mediplus.smarthajiri.com/");
-  // Config.setHomeUrl("https://medibiz.xelwel.com");
+  // Config.setHomeUrl("https://mediplus.smarthajiri.com/");
+  Config.setHomeUrl("https://medibiz.xelwel.com");
 
   // Change login image
   Config.setLoginImage("assets/logo/logo.png");
@@ -55,8 +55,8 @@ void main() async {
   Config.setSplashImage("assets/logo/logo.png");
 
   // App Name and version
-  Config.setApkName("Mediplus");
-  Config.setAppVersion("3.4.6");
+  Config.setApkName("Medibiz");
+  Config.setAppVersion("3.4.7");
 
   Future.delayed(Duration(seconds: 2), () {
     Connectivity().onConnectivityChanged.listen((result) {
